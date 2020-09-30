0821c972f3c0a5f625b6bf4ef46a126dadbaf569
Hiddenalt Code <admin@hiddenalt.ru>
2020-09-30 17:05:14 +0300

    utf8mb4 support for banks_list table

--- database/migrations/2020_06_01_000006_create_banks_list_table.php
+++ database/migrations/2020_06_01_000006_create_banks_list_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 class CreateBanksListTable extends Migration
@@ -23,6 +24,9 @@ class CreateBanksListTable extends Migration
 
             $table->foreign('conversation_id')->references('id')->on('conversations')->onDelete('cascade');
         });
+
+        // Make utf8mb4 for emoji support
+        DB::unprepared('ALTER TABLE `banks_list` CONVERT TO CHARACTER SET utf8mb4');
     }
 
     /**
@@ -33,5 +37,6 @@ class CreateBanksListTable extends Migration
     public function down()
     {
         Schema::dropIfExists('banks_list');
+        DB::unprepared('ALTER TABLE `banks_list` CONVERT TO CHARACTER SET utf8');
     }
 }
