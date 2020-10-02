07f65c4a684d4f061a5dcfac6008ac77ae978655
Hiddenalt Code <admin@hiddenalt.ru>
2020-10-02 23:49:06 +0300

    ConversationProxy trait

--- app/Bot/ConversationHelper/ConversationProxy.php
+++ app/Bot/ConversationHelper/ConversationProxy.php
@@ -4,14 +4,37 @@
 namespace App\Bot\ConversationHelper;
 
 
+use Closure;
+use Exception;
+use Illuminate\Support\Facades\Log;
+
 trait ConversationProxy {
 
-//    public function tryOrSay(callable $method, callable $ifError){
-//        try{
-//            $method();
-//        }catch(\Exception $e){
-//
-//        }
-//    }
+    /**
+     * Try-catch behaviour for conversations.
+     * @param callable $method
+     * @param callable $ifError
+     * @return mixed
+     */
+    public function try(callable $method, callable $ifError){
+        try {
+            return (Closure::bind($method, $this))();
+        } catch(Exception $e){
+            return (Closure::bind($ifError, $this))($e);
+        }
+    }
+
+    /**
+     * @param callable $method
+     * @param string $errorPrefix
+     * @return mixed
+     */
+    public function tryOrSayErrorAndMoveBack(callable $method, string $errorPrefix){
+        return $this->try($method, function(Exception $e) use($errorPrefix){
+            $this->say($errorPrefix . $e->getMessage());
+            Log::error(self::class . ": conversation error: " . $e->getMessage() . ", trace:\n" . $e->getTraceAsString());
+            $this->moveBack();
+        });
+    }
 
 }
